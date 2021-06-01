8307fb2c47af63e0d13d3b64e33fef12027b9890
vdo <vdo@greyfaze.net>
2021-06-01 17:49:21 +0200

    Change regex to loose semver

--- get-semver-and-branch.sh
+++ get-semver-and-branch.sh
@@ -6,12 +6,12 @@ regexString=$1
 semanticVersion=''
 branchName=''
 
-if [ $regexString ]; then 
-    branchName=$(echo $(git branch -a | grep $regexString | grep -E '([0-9]+\.){2}[0-9]+' | sort --version-sort | tail -n 1 | sed 's@.*/@@'))
-    semanticVersion=$(echo $(git branch -a | grep $regexString | egrep -o '([0-9]+\.){2}[0-9]+' | sort --version-sort | tail -n 1))
-else 
-    branchName=$(echo $(git branch -a | grep -E '([0-9]+\.){2}[0-9]+' | sort --version-sort | tail -n 1 | sed 's@.*/@@'))
-    semanticVersion=$(echo $(git branch -a | egrep -o '([0-9]+\.){2}[0-9]+' | sort --version-sort | tail -n 1))
+if [ $regexString ]; then
+    branchName=$(git for-each-ref --format='%(refname:short)' refs/heads/ | grep $regexString | grep -E '([0-9]+\.){1}[0-9].?{1}[0-9]?+' | sort --version-sort | tail -n 1 | sed 's@.*/@@')
+    semanticVersion=$(git for-each-ref --format='%(refname:short)' refs/heads/ | grep $regexString | egrep -o '([0-9]+\.){1}[0-9].?{1}[0-9]?+' | sort --version-sort | tail -n 1)
+else
+    branchName=$(git for-each-ref --format='%(refname:short)' refs/heads/ | grep -E '([0-9]+\.){1}[0-9].?{1}[0-9]?+' | sort --version-sort | tail -n 1 | sed 's@.*/@@')
+    semanticVersion=$(git for-each-ref --format='%(refname:short)' refs/heads/ | egrep -o '([0-9]+\.){1}[0-9].?{1}[0-9]?+' | sort --version-sort | tail -n 1)
 fi
 
-echo '{"semanticVersion": "'$semanticVersion'", "branchName": "'$branchName'"}' 
+echo '{"semanticVersion": "'$semanticVersion'", "branchName": "'$branchName'"}'
